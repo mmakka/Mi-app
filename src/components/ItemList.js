dcbe285f1ae32b2266ca6fe37bdbd080d3802e89
macarena sotelo <macarenasotelo1@gmail.com>
2022-11-23 15:39:47 -1100

    cambios

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,7 +4,7 @@ const ItemList = ({ productos }) => {
   return (
     <ul className="flex items-center justify-center">
       {productos.map((producto) => (
-        <Item key={producto}/>
+        <Item key= {producto.id}producto = {producto}/>
       ))}
     </ul>
   );
